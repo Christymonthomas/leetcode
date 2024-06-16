6fe67668fd18d14e154a7bca4c2f05204335ad70
Christy <christymonthomas1999@gmail.com>
2024-06-16 11:34:40 +0530

    Merge sorted array sol 2

--- Array/mergeSortedArray.js
+++ Array/mergeSortedArray.js
@@ -1,6 +1,30 @@
 // Given two sorted array of integers.
 //Create a func that return an array which is sorted+ merge of both arrays
 
+// function mergeSortedArray(arr1, arr2) {
+//   if (arr1.length === 0) {
+//     return arr2;
+//   }
+//   if (arr2.length === 0) {
+//     return arr1;
+//   }
+//   let arr1Element = arr1[0];
+//   let arr2Element = arr2[0];
+//   let i = 0;
+//   let j = 0;
+//   let result = [];
+//   while (arr1Element || arr2Element) {
+//     if (arr2Element === undefined || arr1Element < arr2Element) {
+//       result.push(arr1Element);
+//       arr1Element = arr1[++i];
+//     } else {
+//       result.push(arr2Element);
+//       arr2Element = arr2[++j];
+//     }
+//   }
+//   return result;
+// }
+
 function mergeSortedArray(arr1, arr2) {
   if (arr1.length === 0) {
     return arr2;
@@ -8,19 +32,6 @@ function mergeSortedArray(arr1, arr2) {
   if (arr2.length === 0) {
     return arr1;
   }
-  let arr1Element = arr1[0];
-  let arr2Element = arr2[0];
-  let i = 0;
-  let j = 0;
-  let result = [];
-  while (arr1Element || arr2Element) {
-    if (arr2Element === undefined || arr1Element < arr2Element) {
-      result.push(arr1Element);
-      arr1Element = arr1[++i];
-    } else {
-      result.push(arr2Element);
-      arr2Element = arr2[++j];
-    }
-  }
-  return result;
+  let result = [...arr1, ...arr2];
+  return result.sort((a, b) => a - b);
 }
