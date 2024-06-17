13856a71b725fb50784d11775bd26a2f2db8ffe5
Christy <christymonthomas1999@gmail.com>
2024-06-17 22:54:41 +0530

    Two sum solution using hashmap

--- Array/twoSum.js
+++ Array/twoSum.js
@@ -25,22 +25,38 @@
 // -109 <= target <= 109
 // Only one valid answer exists.
 
-//   SOLUTION -1
+//   SOLUTION -1 (Brute Force).
 //   ***********
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
-  for (let p1 = 0; p1 < nums.length; p1++) {
-    numberToFind = target - nums[p1];
+// var twoSum = function (nums, target) {
+//   for (let p1 = 0; p1 < nums.length; p1++) {
+//     numberToFind = target - nums[p1];
 
-    for (let p2 = p1 + 1; p2 < nums.length; p2++) {
-      if (numberToFind === nums[p2]) {
-        return [p1, p2];
-      }
+//     for (let p2 = p1 + 1; p2 < nums.length; p2++) {
+//       if (numberToFind === nums[p2]) {
+//         return [p1, p2];
+//       }
+//     }
+//   }
+//   return null;
+// };
+
+// SOLUTION -2 (Using HashMap)
+
+var twoSum = function (nums, target) {
+  const numsMap = {};
+  for (let p = 0; p < nums.length; p++) {
+    let currentMapVal = numsMap[nums[p]];
+    if (currentMapVal >= 0) {
+      return [currentMapVal, p];
+    } else {
+      let numberToFind = target - nums[p];
+      numsMap[numberToFind] = p;
     }
   }
-  return null;
+  return nul;
 };
